a0a02aefec43a1f3a53bd8491fc356121952a52d
Chrissy Albert <chrissy@peerlogic.com>
2022-01-04 12:40:35 -0700

    finish module 7, fix cant find field allProducts on ROOT_QUERY object error

--- sick-fits/frontend/lib/paginationField.js
+++ sick-fits/frontend/lib/paginationField.js
@@ -7,9 +7,34 @@ export default function paginationField() {
       console.log({ existing, args, cache });
       const { skip, first } = args;
       const data = cache.readQuery({ query: PAGINATION_QUERY });
-      const count = data?._allProductsMeta?.count
+      const count = data?._allProductsMeta?.count;
+      const page = skip / first + 1;
+      const pages = Math.ceil(count / first);
       console.log(data);
+      const items = existing.slice(skip, skip + first).filter((x) => x);
+      if (items.length && items.length !== first && page === pages) {
+        return items;
+      }
+      if (items.length !== first) {
+        return false;
+      }
+      if (items.length) {
+        console.log(
+          `There are ${items.length} items in the cache! Sending them to apollo`
+        );
+        return items;
+      }
+      return false;
     },
-    merge() {},
+    merge(existing, incoming, { args }) {
+      const { skip, first } = args;
+      console.log(`Merging items from the network ${incoming.length}`);
+      const merged = existing ? existing.slice(0) : [];
+      for (let i = skip; i < skip + incoming.length; i++) {
+        merged[i] = incoming[i - skip];
+      }
+      console.log(merged);
+      return merged;
+    },
   };
 }
